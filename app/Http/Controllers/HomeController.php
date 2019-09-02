59c674d7caba1f2f86f43ac727d7eac8c1d993da
Gitea <gitea@fake.local>
2019-09-02 18:23:31 +0530

    Added reporting branch

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Kpi;
 use App\Category;
 use Illuminate\Http\Request;
+use App\Performance;
 
 class HomeController extends Controller
 {
@@ -30,6 +31,36 @@ class HomeController extends Controller
     }
 
     public function test() {
+
+        $kpis = Kpi::with('performance')->where('category', 'Crude Oil')->get();
+
+        $total = [
+            "planned" => [],
+            "actual" => [],
+            "unit" => "",
+            "name" => "",
+        ];
+
+        foreach ($kpis as $kpi) {
+            
+            $planned = $kpi->performance->pluck('planned')->toArray();
+            $actual = $kpi->performance->pluck('actual')->toArray();
+
+            $total["unit"] = $kpi->unit;
+            $total["name"] = $kpi->category . " Total";
+            $total["tag"] = $kpi->category . "_tot";
+
+            $total["planned"] = array_map(function () {
+                return array_sum(func_get_args());
+            }, $planned, $total["planned"] );
+
+            $total["actual"] = array_map(function () {
+                return array_sum(func_get_args());
+            }, $actual, $total["actual"] );
+
+        }
+
+        return $total;
 
         $numbers = collect([1, 2, 3, 4, 5, 6, 7, 8 ]);
         $numbers = $numbers->pad(12, 0);
